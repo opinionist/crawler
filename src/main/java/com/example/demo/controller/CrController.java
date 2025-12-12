15ab978a1b7978589458d7a16b44483ec24bdb83
minsung gwak <131960511+opinionist@users.noreply.github.com>
2025-12-12 09:46:27 +0900

    크롤링 준비

--- src/main/java/com/example/demo/controller/CrController.java
+++ src/main/java/com/example/demo/controller/CrController.java
@@ -1,11 +1,15 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.Product;
 import com.example.demo.service.CrService;
 import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.devtools.SeleniumCdpConnection;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,7 +18,12 @@ public class CrController {
     private final CrService crService;
 
     @GetMapping("/test")
-    public String test() {
-        return "CR Controller is working!";
+    public List<Product> test(@RequestParam String keyword, @RequestParam(defaultValue = "1") int page) {
+        try {
+            return crService.getShoppingData(keyword, page);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
+    }
 }
